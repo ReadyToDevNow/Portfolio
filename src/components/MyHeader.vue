078f57df4347ed7247d08afa031dcddf12efeb2a
ReadyToDevNow <learntolearn0@gmail.com>
2025-05-07 06:31:38 +0200

    feat(header): transform logos to button link

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,3 +1,11 @@
+<script setup>
+function linkedinLink() {
+  window.open('https://www.linkedin.com/in/mehdi-lenicque/', '_blank')
+}
+function githubLink() {
+  window.open('https://github.com/ReadyToDevNow', '_blank')
+}
+</script>
 <template>
   <div class="flex p-4 gap-4 items-center justify-between">
     <div class="flex gap-4">
@@ -7,8 +15,18 @@
       <router-link :to="{ path: 'projects' }">Portfolio</router-link>
     </div>
     <div class="flex gap-4">
-      <img src="@/assets/icons/icon-github.svg" alt="icon-github" class="size-16" />
-      <img src="@/assets/icons/icon-link.svg" alt="icon-linkedin" class="size-16" />
+      <img
+        @click="githubLink"
+        src="@/assets/icons/icon-github.svg"
+        alt="icon-github"
+        class="size-16"
+      />
+      <img
+        @click="linkedinLink"
+        src="@/assets/icons/icon-link.svg"
+        alt="icon-linkedin"
+        class="size-16"
+      />
     </div>
   </div>
 </template>
