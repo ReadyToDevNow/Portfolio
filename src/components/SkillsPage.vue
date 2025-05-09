cd649b2cd095e6ea6794fccdbaead33826d15aae
ReadyToDevNow <learntolearn0@gmail.com>
2025-05-09 03:13:29 +0200

    feat(Skills): Create a button for each skill

--- src/components/SkillsPage.vue
+++ src/components/SkillsPage.vue
@@ -1,11 +1,33 @@
 <template>
   <main class="bg-gray-300 flex flex-grow flex-col items-center justify-center">
-    <div class="flex flex-col mb-4">
-      <span class="text-3xl">Mes skills </span>
+    <div class="flex flex-col mb-4 items-center">
+      <span class="text-4xl p-4">Mes skills </span>
+      <span class="p-4">Barre de recherche</span>
+      <div class="grid grid-cols-3 gap-6 p-4">
+        <button>HTML 5</button>
+        <button>CSS3</button>
+        <button>Javascript</button>
+        <button>Vue.Js</button>
+        <button>Vite</button>
+        <button>Tailwind</button>
+        <button>Pinia</button>
+        <button>Canvas</button>
+        <button>NodeJs</button>
+        <button>Windows</button>
+        <button>React</button>
+        <button>Github</button>
+        <button>Githlab</button>
+        <button>Primevue</button>
+        <button>Excel</button>
+      </div>
     </div>
   </main>
 </template>
 
 <script setup></script>
 
-<style></style>
+<style scoped>
+button {
+  background-color: rgba(0, 0, 255, 0.393);
+}
+</style>
