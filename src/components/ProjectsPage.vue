1e68f553bfe2b8c8df649b7bc8af5a7b9fc89083
ReadyToDevNow <learntolearn0@gmail.com>
2025-05-26 19:54:26 +0200

    feat(Projects): Display cards with v-for directive

--- src/components/ProjectsPage.vue
+++ src/components/ProjectsPage.vue
@@ -6,36 +6,45 @@
         Ci-dessous, quelques exemples de réalisations effectuées durant ma vie de developpeur
       </span>
     </div>
+    <div class="grid grid-cols-4 bg p-2">
+      <div v-for="project in projects" :key="project" class="flex flex-col items-center p-4">
+        <span class="font-bold"> {{ project.title }}</span>
+        <span class="mb-4"> {{ project.technologie }}</span>
+        <img :src="project.image" :alt="project.title" class="h-49" />
+      </div>
+    </div>
     <!-- Les Cards Projects-->
     <div class="grid grid-cols-4 bg p-2">
       <div class="flex flex-col items-center p-4">
-        <span class="font-bold">Titre</span>
-        <span class="mb-4">Technologie utilisé</span>
-        <img src="@/assets/poke.png" class="" alt="" />
+        <span class="font-bold">Jeu Pokémon</span>
+        <span class="mb-4">Vue.js | JS | Tyradex API | tailwindcss</span>
+        <img src="@/assets/poke.png" class="h-49" alt="" />
       </div>
       <!-- 2ème projet-->
       <div class="flex flex-col items-center p-4">
         <span class="font-bold">Titre</span>
         <span class="mb-4">Technologie utilisé</span>
-        <img src="@/assets/poke.png" class="" alt="" />
+        <img src="@/assets/novakit.png" class="h-49" alt="" />
       </div>
       <!--3ème projet-->
       <div class="flex flex-col items-center p-4">
         <span class="font-bold">Titre</span>
         <span class="mb-4">Technologie utilisé</span>
-        <img src="@/assets/poke.png" class="" alt="" />
+        <img src="@/assets/poke.png" class="h-49" alt="" />
       </div>
       <!--4 ème projet-->
       <div class="flex flex-col items-center p-4">
         <span class="font-bold">Titre</span>
-        <span>Technologie utilisé</span>
-        <img src="@/assets/poke.png" class="" alt="" />
+        <span class="mb-4">Technologie utilisé</span>
+        <img src="@/assets/portfolio.png" class="h-49" alt="" />
       </div>
     </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { projects } from '@/services/MyProjects'
+</script>
 
 <style>
 .bg {
--- src/services/MyProjects.js
+++ src/services/MyProjects.js
@@ -1,6 +1,30 @@
 export const projects = [
   {
-    name: 'Pokedex',
-    image: '@/assets/poke.png',
+    title: 'Jeu Pokemon',
+    technologie: 'Vue.js | JS | Tyradex API',
+    image: 'src/assets/poke.png',
+    description:
+      ' Un jeu de mémoire ou le but est de trouver le noms de tout les pokémons le plus rapidement possible avec un système de temps de score et de classements. ',
+  },
+  {
+    title: 'NovaKit',
+    technologie: 'Vue.js | JS | HTML| CSS',
+    image: 'src/assets/novakit.png',
+    description:
+      "Market place d'assets digital pour créer un site ou un jeux vidéos avec des ressources simple d'accès ",
+  },
+  {
+    title: 'NovaKit',
+    technologie: 'Vue.js | JS | HTML| CSS',
+    image: 'src/assets/novakit.png',
+    description:
+      "Market place d'assets digital pour créer un site ou un jeux vidéos avec des ressources simple d'accès ",
+  },
+  {
+    title: 'Portofilio',
+    technologie: 'Vue.js | JS | HTML| Tailwindcss',
+    image: 'src/assets/portfolio.png',
+    description:
+      "L'endroit'sur lequel vous vous trouvez actuellement. Ce musée vous permettra d'en apprendre plus sur moi ",
   },
 ]
