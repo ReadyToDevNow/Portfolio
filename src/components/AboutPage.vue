ffe70051eeb2be580b76b81ef43efea9eb677d8e
ReadyToDevNow <learntolearn0@gmail.com>
2025-09-11 16:19:25 +0200

    feat(design): text update and reponsive

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,28 +1,26 @@
 <template>
   <main class="flex flex-col items-center justify-center border">
-    <h2 class="text-3xl mb-4">A Propos</h2>
-    <div class="grid grid-cols-2">
-      <div class="flex flex-col text-xl text-justify ml-40 p-2">
+    <h2 class="text-3xl mb-2 mt-2">A Propos</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:ml-40 p-8 items-center justify-center">
+      <div class="flex flex-col text-xl text-justify">
         <span class="mb-4"
           >Passionné par la tech, la création et le gaming depuis toujours, j'ai décidé il y a cinq
-          ans de me reconvertir dans le développement web. J'ai commencé par une année à Epitech, où
-          j'ai appris le langage C et les bases de la programmation. En parallèle, j'ai développé
-          des petits jeux avec Unity, ce qui m'a donné envie d'en apprendre plus.</span
+          ans de me reconvertir dans le développement web. En formation chez Epitech, où j'ai appris
+          le langage C et les bases de la programmation. En parallèle, j'ai développé des petits
+          jeux avec Unity.</span
         >
 
         <span class="mb-4"
-          >J'ai ensuite continué à me former de manière autodidacte grâce à des formations en ligne
-          (OpenClassrooms, Udemy, YouTube…). J'ai appris HTML, CSS, JavaScript et React, puis je me
-          suis spécialisé en Vue.js.
+          >La formation continue grâce à des formations en ligne (OpenClassrooms, Udemy, YouTube…).
+          J'ai appris HTML, CSS, JavaScript et React, puis je me suis spécialisé en Vue.js.
         </span>
         <span class="mb-4">
-          Depuis Janvier 2024 j'ai eu la chance de progresser en tant que Développeur front-end au
-          coté d'un senior avec plus de 10 années d'expertise. Ensemble nous avons construit un
-          projet concret en Vue.js: un dashboard permettant de visualiser et de gerer un parc
-          informatique.
+          En Janvier 2024 j'ai eu la chance de progresser en tant que Développeur front-end au coté
+          d'un senior avec plus de 10 années d'expertises. Ensemble nous avons construit un
+          dashboard permettant de visualiser et de gérer un parc informatique.
         </span>
         <span class="mb-4">
-          Je suis curieux, et j'aime créer des interfaces modernes qui répondent à des besoins
+          Je suis curieux, j'aime créer et trouver des solutions qui répondent à des besoins
           concrets. Je continue d'apprendre chaque jour et cherche à m'impliquer dans des projets
           .</span
         >
