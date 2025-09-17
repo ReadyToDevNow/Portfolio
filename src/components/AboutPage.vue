67cfec46e61c61dd773c0ef5ae5c9152280e9246
ReadyToDevNow <learntolearn0@gmail.com>
2025-09-17 10:20:41 +0200

    feat(design): delete border

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col items-center justify-center border">
+  <main class="flex flex-col items-center justify-center">
     <h2 class="text-3xl mt-2">A Propos</h2>
     <div class="grid grid-cols-1 lg:grid-cols-2 lg:ml-20 p-8 items-center justify-center">
       <div class="flex flex-col text-xl text-justify">
